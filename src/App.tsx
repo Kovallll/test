0eb4490c93748129910c80e34ab13e612cd9673c
Danila <kovallll.e@gmail.com>
2024-09-24 17:50:17 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       </div>
       <div style={{display: 'flex', flexDirection: 'column'}}>
       <h2>isWithHolidays</h2>
-      <Calendar isWithHolidays={true}/>
+      <Calendar isWithHoliday={true}/>
       </div>
       <div style={{display: 'flex', flexDirection: 'column'}}>
       <h2>isWithInput</h2>
@@ -36,8 +36,20 @@ function App() {
       </div>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
       <div style={{display: 'flex', flexDirection: 'column'}}>
-      <h2>isWithMinMax</h2>
+      <h3>isWithMinMax</h3>
       <Calendar isWithMinMax={true} minDate={'11/09/2024'} maxDate={'11/10/2024'}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h3>isWithMinMax & isWithInput</h3>
+      <Calendar isWithInput={true} isWithMinMax={true} minDate={'11/09/2024'} maxDate={'11/10/2024'}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h3>isWithMinMax & isWithInput & isWithTodos</h3>
+      <Calendar isWithInput={true} isWithTodos={true} isWithMinMax={true} minDate={'11/09/2024'} maxDate={'11/10/2024'}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h3>isWithMinMax & isWithInput & isWithTodos & isWithHoliday</h3>
+      <Calendar isWithInput={true} isWithTodos={true} isWithHoliday={true} isWithMinMax={true} minDate={'11/09/2024'} maxDate={'11/10/2024'}/>
       </div>
       </div>
       </header>
