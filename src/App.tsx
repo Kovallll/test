f8344b87caea20607d8e410b85bef8392e683be4
Danila <kovallll.e@gmail.com>
2024-09-24 17:44:36 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Calendar isWithInput={true} isWithRange={true} isWithTodos={true}/>
+      <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithTodos</h2>
+      <Calendar isWithTodos={true}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithRange</h2>
+      <Calendar isWithRange={true}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithRange & isWithInput</h2>
+      <Calendar isWithRange={true} isWithInput={true}/>
+      </div>
+      
+      </div>
+      <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithStartSunday</h2>
+      <Calendar isWithStartSunday={true}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithHolidays</h2>
+      <Calendar isWithHolidays={true}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithInput</h2>
+      <Calendar isWithInput={true}/>
+      </div>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <h2>isWithMinMax</h2>
+      <Calendar isWithMinMax={true} minDate={'11/09/2024'} maxDate={'11/10/2024'}/>
+      </div>
+      </div>
       </header>
     </div>
   );
