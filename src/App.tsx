4e16abe41107aff1ec40302852429902ceed2102
Danila <kovallll.e@gmail.com>
2024-09-24 17:32:01 +0300

    add calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Calendar } from 'date-picker-calendar-react'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Calendar isWithInput={true} isWithRange={true} isWithTodos={true}/>
       </header>
     </div>
   );
